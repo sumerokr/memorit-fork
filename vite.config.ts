a040fb371bb7661b106691d43d2eadd301678220
Aleksandr Sadykov <sumerokr@gmail.com>
2023-02-27 22:32:33 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,36 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Memorit",
+        short_name: "Memorit",
+        description: "Improve your memory",
+        theme_color: "#6366f1",
+        background_color: "#c7d2fe",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
