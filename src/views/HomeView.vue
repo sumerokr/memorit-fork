8e064dd368ae000fecd49e5912fda1525625a11d
Aleksandr Sadykov <sumerokr@gmail.com>
2023-02-28 11:27:34 +0100

    welcome page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Welcome</h1>
-    <!-- <p><RouterLink :to="{ name: 'sets' }">Sets</RouterLink></p> -->
+  <div class="flex-grow bg-neutral-100 p-4">
+    <h1 class="text-2xl mb-4">Welcome</h1>
+    <p>
+      Go to
+      <RouterLink :to="{ name: 'sets' }" class="text-indigo-500"
+        >Sets</RouterLink
+      >
+    </p>
   </div>
 </template>
