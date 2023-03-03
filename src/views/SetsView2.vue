e1d2b9fe9739aafaf934b3fce4cbdd1180d61195
Aleksandr Sadykov <sumerokr@gmail.com>
2023-03-03 21:23:40 +0100

    search, pager, url

--- src/views/SetsView2.vue
+++ src/views/SetsView2.vue
@@ -1,61 +1,60 @@
 <script setup lang="ts">
-import { ref, nextTick } from "vue";
+import { ref, computed, nextTick, watch } from "vue";
 import { useAsyncState, watchDebounced } from "@vueuse/core";
 import { cardSetAPI } from "@/services/index";
 import CardSetList from "@/components/CardSetList.vue";
 import BaseButton from "@/components/BaseButton.vue";
+import { useRoute, useRouter } from "vue-router";
+import pickBy from "lodash/pickBy";
+import pick from "lodash/pick";
+import omit from "lodash/omit";
+import mapValues from "lodash/mapValues";
+
+const route = useRoute();
+const router = useRouter();
+
+const currentNavigationParams = computed<
+  Partial<{ before?: string; after?: string; query?: string }>
+>(() => {
+  const { before, after, query } = route.query;
+  const mapped = mapValues({ before, after, query }, (item) => {
+    const itemResolved = Array.isArray(item) ? item[0] : item;
+    return itemResolved ? itemResolved.trim() : "";
+  });
+  const picked = pickBy(mapped, (item) => {
+    return item.length > 0;
+  });
+  return picked.before && picked.after ? omit(picked, "before") : picked;
+});
 
 const { /* isReady, isLoading, */ execute, state, error } = useAsyncState(
   cardSetAPI.getAll,
   null,
   {
+    immediate: false,
     throwError: true,
     resetOnExecute: false,
   }
 );
 
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-};
-
-//#region pager
-const onPrev = () => {
-  if (!state.value?.before) return;
-  execute(0, {
-    before: state.value?.before,
-    ...(query.value ? { query: query.value } : {}),
-  });
-  scrollToTop();
-};
-
-const onNext = () => {
-  if (!state.value?.after) return;
-  execute(0, {
-    after: state.value?.after,
-    ...(query.value ? { query: query.value } : {}),
-  });
-  scrollToTop();
-};
-//#endregion
-
-//#region search
-const isSearchVisible = ref(false);
 const queryEl = ref<HTMLInputElement | null>(null);
-const query = ref(null);
+const query = ref<string>(currentNavigationParams.value.query ?? "");
+const isSearchVisible = ref<boolean>(!!query.value);
 watchDebounced(
   query,
-  () => {
-    execute(0, query.value ? { query: query.value } : null);
+  (newQuery, oldQuery) => {
+    if (newQuery) {
+      router.replace({ query: { query: newQuery } });
+    } else if (oldQuery) {
+      router.replace({ query: omit(route.query, "query") });
+    }
   },
   { debounce: 500 }
 );
 
 const onSearch = async () => {
   if (isSearchVisible.value) {
-    query.value = null;
+    query.value = "";
     isSearchVisible.value = false;
   } else {
     isSearchVisible.value = true;
@@ -63,11 +62,50 @@ const onSearch = async () => {
     queryEl.value?.focus();
   }
 };
-//#endregion
+
+const nextNavigationParams = computed<
+  Partial<{ before?: string; after?: string; query?: string }>
+>(() => {
+  return pickBy(
+    {
+      query: query.value,
+      before: state.value?.before,
+      after: state.value?.after,
+    },
+    (param) => param && param.trim().length > 0
+  );
+});
+
+watch(
+  currentNavigationParams,
+  (next, prev) => {
+    // do nothing if unneeded before parameter was removed
+    if (!next.before && prev?.before) {
+      return;
+    }
+    execute(0, currentNavigationParams.value);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  },
+  { immediate: true }
+);
+
+watch(
+  nextNavigationParams,
+  (next) => {
+    // remove unneeded before parameter if there are no more pages before
+    if (!next.before && currentNavigationParams.value.before) {
+      router.replace({ query: omit(route.query, "before") });
+    }
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
-  <div class="flex-grow bg-neutral-100 p-4 pb-24">
+  <div class="flex flex-col flex-grow bg-neutral-100 p-4 pb-24">
     <div class="flex justify-between gap-4 mb-4">
       <h1 class="text-2xl">Card sets</h1>
       <div class="relative">
@@ -99,23 +137,37 @@ const onSearch = async () => {
         v-if="state.before || state.after"
         class="flex justify-end gap-4 mt-4"
       >
-        <BaseButton
-          before="chevron_left"
+        <RouterLink
+          v-if="state.before"
+          :to="{
+            name: 'sets',
+            query: pick(nextNavigationParams, ['query', 'before']),
+          }"
+          class="inline-flex gap-2 pr-6 pl-4 py-2 items-center rounded-2xl justify-center bg-indigo-100"
           :disabled="!state.before"
-          @click="onPrev"
-          >prev</BaseButton
+          ><span class="material-symbols-outlined text-xl leading-none"
+            >chevron_left</span
+          >prev</RouterLink
         >
+        <BaseButton v-else before="chevron_left" disabled>prev</BaseButton>
 
-        <BaseButton
-          after="chevron_right"
-          :disabled="!state.after"
-          @click="onNext"
-          >next</BaseButton
+        <RouterLink
+          v-if="state.after"
+          :to="{
+            name: 'sets',
+            query: pick(nextNavigationParams, ['query', 'after']),
+          }"
+          class="inline-flex gap-2 pr-4 pl-6 py-2 items-center rounded-2xl justify-center bg-indigo-100"
+          before="chevron_left"
+          >prev<span class="material-symbols-outlined text-xl leading-none"
+            >chevron_right</span
+          ></RouterLink
         >
+        <BaseButton v-else after="chevron_right" disabled>next</BaseButton>
       </div>
     </template>
 
-    <p v-else-if="state && !state.data.length">
+    <div class="my-auto text-center" v-else-if="state && !state.data.length">
       You have no card sets yet.
       <RouterLink
         :to="{ name: 'new-card-set' }"
@@ -126,7 +178,7 @@ const onSearch = async () => {
           add
         </span>
       </RouterLink>
-    </p>
+    </div>
 
     <RouterLink
       class="fixed right-4 bottom-4 p-4 flex rounded-2xl bg-indigo-200 shadow-md"
