aab5a70e85d501c832fbf48ec7e24a9ca4dfb614
Aleksandr Sadykov <sumerokr@gmail.com>
2023-04-19 18:19:56 +0200

    generate!

--- functions/api/helloworld.ts
+++ functions/api/helloworld.ts
@@ -2,74 +2,123 @@ interface Env {
   OPENAI_API_KEY: string;
 }
 
-export const onRequest: PagesFunction<Env> = async ({ env }) => {
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      max_tokens: 1000,
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are the rest api endpoint that responds only with JSON data.",
-        },
-        {
-          role: "user",
-          content: {
-            topic: "Formula 1",
-            cards: 3,
-            front: {
-              term: "year",
-            },
-            back: {
-              term: "champion's name",
-              lang: "en",
-            },
+export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
+  const formData = await request.formData();
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are the rest api endpoint that responds only with JSON data.",
           },
-        },
-        {
-          role: "assistant",
-          content: [
-            [2007, "Kimi Räikkönen"],
-            [2008, "Lewis Hamilton"],
-            [2009, "Jenson Button"],
-          ],
-        },
-        {
-          role: "user",
-          content:
-            '{"topic":"Most popular Frontend development terms","cards":3,"front":{"term":"term","lang":"ru"},"back":{"term":"translation","lang":"en"}}',
-        },
-        {
-          role: "assistant",
-          content:
-            '[["Вёрстка", "Markup"], ["База данных", "Database"], ["Вебсайт", "Website"]]',
-        },
-        {
-          role: "user",
-          content:
-            '{"topic":"Most popular capital cities","cards":3,"front":{"term":"city","lang":"ru"},"back":{"term":"country","lang":"ru"}}',
-        },
-      ],
-    }),
-    headers: {
-      Authorization: `Bearer ${env.OPENAI_API_KEY}`,
-      "Content-type": "application/json",
-    },
-  });
+          {
+            role: "user",
+            content: JSON.stringify({
+              topic: "Formula 1 champion names to year",
+              count: 3,
+              front: {
+                term: "year",
+              },
+              back: {
+                term: "champion's name",
+                lang: "en",
+              },
+            }),
+          },
+          {
+            role: "assistant",
+            content: JSON.stringify([
+              [2007, "Kimi Räikkönen"],
+              [2008, "Lewis Hamilton"],
+              [2009, "Jenson Button"],
+            ]),
+          },
+          {
+            role: "user",
+            content: JSON.stringify({
+              topic: "Most popular Web development terms to translation",
+              count: 4,
+              front: { term: "term", lang: "ru" },
+              back: { term: "translation", lang: "en" },
+            }),
+          },
+          {
+            role: "assistant",
+            content: JSON.stringify([
+              ["Запрос", "Request"],
+              ["База данных", "Database"],
+              ["Вебсайт", "Website"],
+              ["Браузер", "Browser"],
+            ]),
+          },
+          {
+            role: "user",
+            content: JSON.stringify({
+              topic: "Most popular capital cities to countries",
+              count: 2,
+              front: { term: "city", lang: "ru" },
+              back: { term: "country", lang: "ru" },
+            }),
+          },
+          {
+            role: "assistant",
+            content: JSON.stringify([
+              ["Вашингтон", "Соединённые Штаты Америки"],
+              ["Лондон", "Англия"],
+            ]),
+          },
+          {
+            role: "user",
+            content: JSON.stringify({
+              topic: formData.get("topic"),
+              count: Number(formData.get("count")) || 10,
+              front: {
+                term: formData.get("front-term"),
+                ...(formData.get("front-lang")
+                  ? { lang: formData.get("front-lang") }
+                  : {}),
+              },
+              back: {
+                term: formData.get("back-term"),
+                ...(formData.get("back-lang")
+                  ? { lang: formData.get("back-lang") }
+                  : {}),
+              },
+            }),
+          },
+        ],
+      }),
+      headers: {
+        Authorization: `Bearer ${env.OPENAI_API_KEY}`,
+        "Content-type": "application/json",
+      },
+    });
 
-  const json = await response.json<any>();
+    const json = await response.json<any>();
 
-  return new Response(
-    JSON.stringify({
-      method: "sendMessage",
-    }),
-    {
+    return new Response(json.choices[0].message.content, {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
       },
-    }
-  );
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: error,
+      }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+  }
 };
--- src/views/GenerateCardSetView.vue
+++ src/views/GenerateCardSetView.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { useAsyncState } from "@vueuse/core";
 import RouterLinkIconButton from "@/components/RouterLinkIconButton.vue";
 import CommonButton from "@/components/CommonButton.vue";
 
-const onSubmit = async () => {
-  fetch("http://localhost:8788/api/helloworld");
+const formEl = ref<HTMLFormElement | null>(null);
+const getFormData = () => new FormData(formEl.value!);
+
+const fetcher = () =>
+  fetch(import.meta.env.VITE_GENERATOR_API, {
+    method: "POST",
+    body: getFormData(),
+  }).then(async (res) => {
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data.error);
+    }
+    return data;
+  });
+
+const { isLoading, isReady, state, error, execute } = useAsyncState(
+  fetcher,
+  null,
+  {
+    immediate: false,
+  }
+);
+
+const onSubmit = () => {
+  if (
+    !formEl.value?.topic.value ||
+    !formEl.value?.count.value ||
+    !formEl.value?.["front-term"].value ||
+    !formEl.value?.["back-term"].value ||
+    formEl.value?.pin.value !== "735"
+  ) {
+    return;
+  }
+
+  execute();
 };
 </script>
 
@@ -19,27 +54,26 @@ const onSubmit = async () => {
       <h1 class="text-2xl">Generate new card set</h1>
     </div>
 
-    <form @submit.prevent="onSubmit">
-      <p class="mb-4">
-        <label for="subject">Subject</label>
+    <form ref="formEl" @submit.prevent="onSubmit">
+      <!-- <p class="mb-4">
         <input
-          id="subject"
+          name="topic"
           class="leading-5 border-2 rounded-2xl p-2 w-full"
           type="text"
           placeholder="Card set name"
           autocomplete="off"
         />
-      </p>
+      </p> -->
       <p class="flex gap-4 mb-4">
         <input
-          id="back"
+          name="topic"
           class="basis-3/4 leading-5 border-2 rounded-2xl p-2 w-full"
           type="text"
-          placeholder="Subject"
+          placeholder="Topic"
           autocomplete="off"
         />
         <input
-          id="back"
+          name="count"
           class="basis-1/4 leading-5 border-2 rounded-2xl p-2 w-full"
           type="text"
           placeholder="Count"
@@ -50,14 +84,14 @@ const onSubmit = async () => {
         <legend>Front</legend>
         <p class="flex gap-4 mb-4">
           <input
-            id="back"
+            name="front-term"
             class="basis-3/4 leading-5 border-2 rounded-2xl p-2 w-full"
             type="text"
             placeholder="Term"
             autocomplete="off"
           />
           <input
-            id="back"
+            name="front-lang"
             class="basis-1/4 leading-5 border-2 rounded-2xl p-2 w-full"
             type="text"
             placeholder="Lang"
@@ -69,14 +103,14 @@ const onSubmit = async () => {
         <legend>Back</legend>
         <p class="flex gap-4 mb-4">
           <input
-            id="back"
+            name="back-term"
             class="basis-3/4 leading-5 border-2 rounded-2xl p-2 w-full"
             type="text"
             placeholder="Term"
             autocomplete="off"
           />
           <input
-            id="back"
+            name="back-lang"
             class="basis-1/4 leading-5 border-2 rounded-2xl p-2 w-full"
             type="text"
             placeholder="Lang"
@@ -84,14 +118,28 @@ const onSubmit = async () => {
           />
         </p>
       </fieldset>
+      <p class="mb-4">
+        <input
+          name="pin"
+          class="leading-5 border-2 rounded-2xl p-2 w-full"
+          type="text"
+          placeholder="PIN"
+          autocomplete="off"
+        />
+      </p>
       <p class="text-right">
         <CommonButton
           before="save"
           class="bg-indigo-500 text-white"
           type="submit"
+          :disabled="isLoading"
           >Generate</CommonButton
         >
       </p>
     </form>
+
+    <div v-if="error">{{ error }}</div>
+    <div v-else-if="isLoading">Loading...</div>
+    <pre v-else-if="isReady && state">{{ JSON.stringify(state, null, 2) }}</pre>
   </div>
 </template>
