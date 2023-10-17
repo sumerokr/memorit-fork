9fc4f07c82d3164a9707e8c5f02c8150a3d83d4f
Aleksandr Sadykov <sumerokr@gmail.com>
2023-10-17 18:25:23 +0200

    sync

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,9 @@ getDBInstance().then((db) => {
   dbVersion.value = db.version;
 });
 
-// const foo1 = "__CF_PAGES_BRANCH__";
-// const foo2 = "process.env.__CF_PAGES_BRANCH__";
-const foo3 = "__CF_PAGES_BRANCH_LIT__";
-const foo4 = "process.env.__CF_PAGES_BRANCH_LIT__";
-const foo5 = process.env.__CF_PAGES_BRANCH__;
-const foo6 = process.env.__CF_PAGES_BRANCH_LIT__;
-
 //@ts-ignore
 window.moo = {
-  // foo1,
-  // foo2,
-  foo3,
-  foo4,
-  foo5,
-  foo6,
+  foo3: "__CF_PAGES_BRANCH__",
 };
 </script>
 
@@ -42,7 +30,7 @@ window.moo = {
       v-if="route.name === 'sets'"
       class="fixed bottom-0 left-1/2 -translate-x-1/2 py-1 px-2 rounded-t-md text-xs bg-white opacity-50"
     >
-      ver: 08101040. DB ver: {{ dbVersion }}, branch: ???
+      ver: 08101040. DB ver: {{ dbVersion }}, branch: __CF_PAGES_BRANCH__
     </div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -55,11 +55,6 @@ export default defineConfig({
     },
   },
   define: {
-    __CF_PAGES_BRANCH__: JSON.stringify(process.env.CF_PAGES_BRANCH),
-    "process.env.__CF_PAGES_BRANCH__": JSON.stringify(
-      process.env.CF_PAGES_BRANCH
-    ),
-    __CF_PAGES_BRANCH_LIT__: process.env.CF_PAGES_BRANCH,
-    "process.env.__CF_PAGES_BRANCH_LIT__": process.env.CF_PAGES_BRANCH,
+    __CF_PAGES_BRANCH__: process.env.CF_PAGES_BRANCH,
   },
 });
